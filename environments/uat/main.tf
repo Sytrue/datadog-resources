f956ff81e90a9ebe28723eff4883f58c67667efc
danieloluwa <danieloyewole@apixio.com>
2025-02-17 22:51:42 -0500

    Initial setup: Datadog module, UAT environment, and GitHub Actions

---
+++ environments/uat/main.tf
@@ -0,0 +1,35 @@
+# Configure Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "cc7e42fb-6f61-4086-9429-b0b5aeee13a2"
+}
+
+# Configure Terraform Backend
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "clqddtfstate"
+    container_name      = "terraform-states"
+    key                 = "uat/terraform.tfstate"
+    use_azuread_auth    = true
+  }
+}
+
+module "datadog_monitors" {
+  source = "../../modules/datadog"
+
+  environment     = "uat"
+  monitor_name    = "high-cpu-usage"
+  alert_message   = "UAT Environment: CPU usage is above threshold"
+  threshold       = 85
+  additional_tags = ["team:platform", "criticality:medium"]
+} 
